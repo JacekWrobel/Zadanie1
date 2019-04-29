1c5f4f3d8400acb3974d1dbe5cd24f211f2fe73f
Jacek <ja77co@wp.pl>
2019-04-29 18:55:15 +0200

    Rozszerzenie treści programu

--- src/UczeSie.java
+++ src/UczeSie.java
@@ -1,5 +1,5 @@
 public class UczeSie {
     public static void main(String[] args) {
-        System.out.println("Uczę się programowania");
+        System.out.println("Uczę się programowania i commitowania");
     }
 }
